d83344ac184a925d4ecce27d96139abee0d11eb2
ShahRishi <shahrishi108@gmail.com>
2026-01-30 21:35:51 +0000

    Deploying to gh-pages from @ ShahRishi/shahrishi.github.io@bb03b2356e446625e3c3b69828dd698c3f1bdd89 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ShahRishi/rishi-shah",
+    "data-repo": "ShahRishi/shahrishi.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
